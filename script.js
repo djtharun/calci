9f08125fcd89c031489969ae0ca61cb03b993d2e
djtharun <djtharun2006@gmail.com>
2026-06-08 21:19:24 +0530

    added backspace

--- script.js
+++ script.js
@@ -23,11 +23,35 @@ let operate = (n1,n2,op) => {
 buttons.addEventListener('click',(event) => {
     if(event.target.matches('button')) {
         if(event.target.textContent==='CE') {
+            // clear button
             screen.textContent = ''
             n1 = n2 = ''
             op = undefined
         }
-        if(ops.includes(event.target.textContent) && op!==undefined && typeof n1 === 'number' && n2!=='') {
+        if(event.target.textContent==='<-') {
+            if(typeof n1 === 'string' && op===undefined) {
+                if(n1.length>1) {
+                    n1 = n1.slice(0,-1)
+                }
+                else {
+                    n1 = ''
+                }
+            }
+            else if(op!==undefined && n2 === '') {
+                op = undefined
+            }
+            else {
+                if(n2.length>1) {
+                    n2 = n2.slice(0,-1)
+                }
+                else {
+                    n2 = ''
+                }
+            }
+            screen.textContent = screen.textContent.slice(0,-1)
+        }
+        else if(ops.includes(event.target.textContent) && op!==undefined && typeof n1 === 'number' && n2!=='') {
+            // when second operator is entered
             n2 = Number(n2)
             n1 = operate(n1,n2,op)
             op = event.target.textContent
@@ -35,11 +59,13 @@ buttons.addEventListener('click',(event) => {
             n2 = ''
         }
         else if(op===undefined && !Number.isNaN(Number(event.target.textContent))) {
+            // when first operand is entered
             n1 += event.target.textContent
             screen.textContent = n1
             console.log('ok1')
         }
         else if(ops.includes(event.target.textContent)) {
+            // when first operator is entered
             if(n1==='') {
                 screen.textContent = 'Enter a number first'
             }
@@ -51,15 +77,17 @@ buttons.addEventListener('click',(event) => {
             console.log('ok2')
         }
         else if(typeof n1 === 'number' && event.target.textContent!=='=') {
+            // when second operand is entered
             n2 += event.target.textContent
             screen.textContent += event.target.textContent
             console.log('ok3')
         }
         else if(event.target.textContent==='=') {
+            // when = is entered
             console.log(n1,n2,op)
             n2 = Number(n2)
-            screen.textContent = operate(n1,n2,op)
-            n1 = n2 = ''
+            screen.textContent = n1 = operate(n1,n2,op)
+            n2 = ''
             op = undefined
         }
     }
